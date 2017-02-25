2983457f4518fcb282aebd9496f9bc9f47f8f314
dweihang <dweihang@hotmail.com>
2017-02-25 09:40:51 +0800

    fix typo

--- src/emq_msgsave_redis_app.erl
+++ src/emq_msgsave_redis_app.erl
@@ -8,7 +8,7 @@
 
 start(_StartType, _StartArgs) ->
   {ok, Sup} = emq_msgsave_redis_sup:start_link(),
-  emq_msgsave_redis:load(),
+  emq_msgsave_redis:load(application:get_all_env()),
   {ok, Sup}.
 
 stop(_State) ->
