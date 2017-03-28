8db49edb147ec23aff274db928cd0ccaf784817f
dweihang <dweihang@hotmail.com>
2017-03-28 15:12:48 +0800

    try log topic prefix

--- src/emq_msgsave_redis.erl
+++ src/emq_msgsave_redis.erl
@@ -6,9 +6,13 @@
 
 -export([handle_message_publish/2]).
 
+-define(ENV(Key, Opts), proplists:get_value(Key, Opts)).
+
 handle_chatroom_message(Message) ->
   emq_msgsave_redis_cli:handle(Message).
 
+get_topic_prefix(Env) ->
+  ?ENV(topic_prefix, Env).
 
 load(Env) ->
   emqttd:hook('message.publish', fun ?MODULE:handle_message_publish/2, [Env]).
@@ -16,10 +20,17 @@ load(Env) ->
 unload() ->
   emqttd:unhook('message.publish', fun ?MODULE:handle_message_publish/2).
 
+
+
 % saved chatroom published mesage to redis
-handle_message_publish(Message = #mqtt_message{topic = <<"cr:", _/binary>>}, _Env) ->
+handle_message_publish(Message = #mqtt_message{topic = <<"cr:", _/binary>>}, Env) ->
+  TopicPrefix = ?ENV(topic_prefix, Env),
+  io:format("test some blood ~s\n", [TopicPrefix]),
   handle_chatroom_message(Message),
   {ok, Message};
 
-handle_message_publish(Message, _Env) ->
+
+handle_message_publish(Message, Env) ->
+  TopicPrefix = ?ENV(topic_prefix, Env),
+  io:format("test some blood ~s\n", [TopicPrefix]),
   {ok, Message}.
